77f9e20176e88601456df6500724628ae0306a40
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-02-21 02:08:21 +0600

    edit and delete done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -18,52 +18,104 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const userCollection = client.db("TasklyDB").collection("users");
-    const taskCollection = client.db("TasklyDB").collection("tasks");
+        const userCollection = client.db("TasklyDB").collection("users");
+        const taskCollection = client.db("TasklyDB").collection("tasks");
 
 
-     // Users APIs
-     app.post("/users", async (req, res) => {
-        // console.log(req.body);
+        // Users APIs
+        app.post("/users", async (req, res) => {
+            // console.log(req.body);
 
-        const query = { email: req.body.email };
-        const existingUser = await userCollection.findOne(query);
-        if (!existingUser) {
-            // Insert new user into the database
-            const result = await userCollection.insertOne(req.body);
+            const query = { email: req.body.email };
+            const existingUser = await userCollection.findOne(query);
+            if (!existingUser) {
+                // Insert new user into the database
+                const result = await userCollection.insertOne(req.body);
+                res.send(result);
+            }
+            else {
+                res.send({ message: "User already exists in the database" });
+            }
+        })
+
+        // Post a new task
+        app.post("/task", async (req, res) => {
+            const newTask = req.body;
+            const query = { email: newTask.email, name: newTask.name };
+            const existingTask = await taskCollection.findOne(query);
+            if (!existingTask) {
+                const result = await taskCollection.insertOne(newTask);
+                res.send(result);
+            }
+            else {
+                res.send({ message: "Task name already exists in the database" });
+            }
+
+        })
+
+        // get all tasks of a specific user
+        app.get("/tasks/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { createdBy: email };
+            const result = await taskCollection.find(query).toArray();
             res.send(result);
-        }
-        else {
-            res.send({ message: "User already exists in the database" });
-        }
-    })
-
-    // Post a new task
-    app.post("/task", async (req, res) => {
-        const newTask = req.body;
-        const query = {email: newTask.email, name: newTask.name};
-        const existingTask = await taskCollection.findOne(query);
-        if(!existingTask){
-            const result = await taskCollection.insertOne(newTask);
+        })
+        // get all todo-tasks of a specific user
+        app.get("/todo-tasks/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { createdBy: email, status: "todo" };
+            const result = await taskCollection.find(query).toArray();
             res.send(result);
-        }
-        else {
-            res.send({ message: "Task name already exists in the database" });
-        }
+        })
+        // get all in-progress-tasks of a specific user
+        app.get("/in-progress-tasks/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { createdBy: email, status: "inProgress" };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        })
+        // get all done-tasks of a specific user
+        app.get("/done-tasks/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { createdBy: email, status: "done" };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        })
 
-    })
+        // update a task of an specific user
+        app.patch("/task/:id", async (req, res) => {
+            const id = req.params.id;
+            const { name, description } = req.body;
+            const query = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    name,
+                    description,
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        // delete a task of an specific user
+        app.delete("/task/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
@@ -71,14 +123,13 @@ async function run() {
 
 
 
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
